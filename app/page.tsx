c9817be35b63fd81a8aba814fa9202707f703333
mernorthzide <mern_k@hotmail.com>
2023-09-01 17:13:13 +0700

    Add shadcn and theme switcher

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,5 @@ import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
-  return <UserButton afterSignOutUrl="/" />;
+  return <div>Hello</div>;
 }
